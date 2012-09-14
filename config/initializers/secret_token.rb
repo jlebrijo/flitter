e5711f41da7aeba8ceb8bc99b113579b4788d4bb
Juanan Lebrijo <jlebrijo@gmail.com>
2012-09-14 15:07:04 +0200

    First commit$ rails flitter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flitter::Application.config.secret_token = '92c9814bd53b2a34765831c6de4009537791b372b3aa0809245aca267b86fd7ad3b31d4605a3cc57269fce9499fb072b1148c705d7e963843f3afeef774e9ac6'
