e9855c9dfc1f35bb4644aef36fd3118212a6ecec
Juanan Lebrijo <jlebrijo@gmail.com>
2012-09-14 23:50:12 +0200

    Styling the update flit form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   before_filter :login_required
   def index
     @flits = current_user.all_flits.reverse
+    @last_flit = current_user.flits.last
   end
 end
